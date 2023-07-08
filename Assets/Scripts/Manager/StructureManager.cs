8b93cbb565aa700a4bc12729441a084213fdfdbf
chollasit-dev <118034127+chollasit-dev@users.noreply.github.com>
2023-07-08 14:32:26 +0700

    Game Manager

---
+++ Assets/Scripts/Manager/StructureManager.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StructureManager : MonoBehaviour
+{
+    [SerializeField] private bool isConstructing;
+    [SerializeField] private bool isDemolishing;
+
+    [SerializeField] private GameObject curBuildingPrefab;
+    [SerializeField] private GameObject buildingParent;
+
+    [SerializeField] private Vector3 curCursorPos;
+
+    public GameObject buildingCursor;
+    public GameObject gridPlane;
+
+    private GameObject ghostBuilding;
+
+    [SerializeField] private GameObject _curStructure; //Currently selected structure
+    public GameObject CurStructure { get { return _curStructure; } set { _curStructure = value; } }
+
+    [SerializeField] private GameObject[] structurePrefab;
+
+    private Camera cam;
+
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+        curCursorPos = Formula.instance.GetCurTilePosition();
+
+        if (isConstructing) //Mode Construct
+        {
+            buildingCursor.transform.position = curCursorPos;
+            gridPlane.SetActive(true);
+        }
+        else //Mode Play
+        {
+            gridPlane.SetActive(false);
+        }
+    }
+
+    public void BeginNewBuildingPlacement(GameObject prefab)    //Button Mapping
+    {
+        isDemolishing = false;
+        isConstructing = true;
+
+        curBuildingPrefab = prefab;
+
+        //Instantiage Ghost Building
+        ghostBuilding = Instantiate(curBuildingPrefab, curCursorPos, Quaternion.identity);
+        ghostBuilding.GetComponent<FindBuildingSite>().Plane.SetActive(true);
+
+        buildingCursor = ghostBuilding;
+        buildingCursor.SetActive(true);
+    }
+}
