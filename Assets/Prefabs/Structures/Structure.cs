22ebbfba3e1c292ce7e8bbf830ac55ee9e5949d3
chollasit-dev <118034127+chollasit-dev@users.noreply.github.com>
2023-07-08 13:34:30 +0700

    Structure & Farm Script

---
+++ Assets/Prefabs/Structures/Structure.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum StructureType
+{
+    road,
+    building,
+    wheat,
+}
+
+public abstract class Structure : MonoBehaviour
+{
+    [SerializeField] private StructureType structureType;
+    public StructureType StructureType { get { return structureType; } set { structureType = value; } }
+
+    [SerializeField] protected bool functional;
+
+    [SerializeField] private string structureName;
+    public string StructureName { get { return structureName; } }
+
+    [SerializeField] protected int hp;
+    public int HP { get { return hp; } set { hp = value; } }
+
+    [SerializeField] private int costToBuild;
+    public int CostToBuild { get { return costToBuild; } }
+
+    [SerializeField] private int id;
+    public int ID { get { return id; } set { id = value; } }
+    void Start()
+    {
+        functional = false;
+        hp = 1;
+    }
+
+    void Update()
+    {
+
+    }
+}
